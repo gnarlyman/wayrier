1fc373f77e21f70023f006c53d6aae37c1b49ddc
Mike Schon <gnarlyman@proton.me>
2022-08-01 18:36:46 -0600

    refactored for single server/port

--- client.py
+++ client.py
@@ -4,130 +4,123 @@ import sys
 import asyncio
 import ecodes
 
+keymap = {
+    "KEY_0": '0',
+    "KEY_1": '1',
+    "KEY_2": '2',
+    "KEY_3": '3',
+    "KEY_4": '4',
+    "KEY_5": '5',
+    "KEY_6": '6',
+    "KEY_7": '7',
+    "KEY_8": '8',
+    "KEY_9": '9',
+    "KEY_A": 'a',
+    "KEY_B": 'b',
+    "KEY_C": 'c',
+    "KEY_D": 'd',
+    "KEY_E": 'e',
+    "KEY_F": 'f',
+    "KEY_G": 'g',
+    "KEY_H": 'h',
+    "KEY_I": 'i',
+    "KEY_J": 'j',
+    "KEY_K": 'k',
+    "KEY_L": 'l',
+    "KEY_M": 'm',
+    "KEY_N": 'n',
+    "KEY_O": 'o',
+    "KEY_P": 'p',
+    "KEY_Q": 'q',
+    "KEY_R": 'r',
+    "KEY_S": 's',
+    "KEY_T": 't',
+    "KEY_U": 'u',
+    "KEY_V": 'v',
+    "KEY_W": 'w',
+    "KEY_X": 'x',
+    "KEY_Y": 'y',
+    "KEY_Z": 'z',
+    "KEY_LEFTSHIFT": Key.shift,
+    "KEY_LEFTCTRL": Key.ctrl,
+    "KEY_LEFTMETA": Key.cmd,
+    "KEY_SPACE": Key.space,
+    "KEY_LEFTALT": Key.alt,
+    "KEY_TAB": Key.tab,
+    "KEY_BACKSPACE": Key.backspace,
+    "KEY_ENTER": Key.enter,
+    "KEY_ESC": Key.esc,
+    "KEY_DELETE": Key.delete,
+    "KEY_UP": Key.up,
+    "KEY_DOWN": Key.down,
+    "KEY_LEFT": Key.left,
+    "KEY_RIGHT": Key.right,
+    "KEY_MINUS": '-',
+    "KEY_EQUAL": '=',
+    "KEY_GRAVE": '`',
+    "KEY_SEMICOLON": ';',
+    "KEY_SLASH": '/',
+    "KEY_BACKSLASH": '\\',
+    "KEY_LEFTBRACE": '[',
+    "KEY_RIGHTBRACE": ']',
+    "KEY_COMMA": ',',
+    "KEY_DOT": '.',
+}
 
-async def keyboard_client(r):
-
-    keymap = {
-        "KEY_0": '0',
-        "KEY_1": '1',
-        "KEY_2": '2',
-        "KEY_3": '3',
-        "KEY_4": '4',
-        "KEY_5": '5',
-        "KEY_6": '6',
-        "KEY_7": '7',
-        "KEY_8": '8',
-        "KEY_9": '9',
-        "KEY_A": 'a',
-        "KEY_B": 'b',
-        "KEY_C": 'c',
-        "KEY_D": 'd',
-        "KEY_E": 'e',
-        "KEY_F": 'f',
-        "KEY_G": 'g',
-        "KEY_H": 'h',
-        "KEY_I": 'i',
-        "KEY_J": 'j',
-        "KEY_K": 'k',
-        "KEY_L": 'l',
-        "KEY_M": 'm',
-        "KEY_N": 'n',
-        "KEY_O": 'o',
-        "KEY_P": 'p',
-        "KEY_Q": 'q',
-        "KEY_R": 'r',
-        "KEY_S": 's',
-        "KEY_T": 't',
-        "KEY_U": 'u',
-        "KEY_V": 'v',
-        "KEY_W": 'w',
-        "KEY_X": 'x',
-        "KEY_Y": 'y',
-        "KEY_Z": 'z',
-        "KEY_LEFTSHIFT": Key.shift,
-        "KEY_LEFTCTRL": Key.ctrl,
-        "KEY_LEFTMETA": Key.cmd,
-        "KEY_SPACE": Key.space,
-        "KEY_LEFTALT": Key.alt,
-        "KEY_TAB": Key.tab,
-        "KEY_BACKSPACE": Key.backspace,
-        "KEY_ENTER": Key.enter,
-        "KEY_ESC": Key.esc,
-        "KEY_DELETE": Key.delete,
-        "KEY_UP": Key.up,
-        "KEY_DOWN": Key.down,
-        "KEY_LEFT": Key.left,
-        "KEY_RIGHT": Key.right,
-        "KEY_MINUS": '-',
-        "KEY_EQUAL": '=',
-        "KEY_GRAVE": '`',
-        "KEY_SEMICOLON": ';',
-        "KEY_SLASH": '/',
-        "KEY_BACKSLASH": '\\',
-        "KEY_LEFTBRACE": '[',
-        "KEY_RIGHTBRACE": ']',
-        "KEY_COMMA": ',',
-        "KEY_DOT": '.',
-    }
-
-    keyboard = kC()
-    while True:
-        data = await r.readline()
-        if data:
-            code, ev_type, value = map(int, data.decode().split())
-            print('keyboard', code, ev_type, value)
-            key = ecodes.KEY[code]
-            print(key)
-            if key not in keymap:
-                continue
-
-            elif value == 1:
-                keyboard.press(keymap[key])
-            elif value == 0:
-                keyboard.release(keymap[key])
+keyboard = kC()
+mouse = mC()
 
 
-async def mouse_client(r):
-    mouse = mC()
-    while True:
-        data = await r.readline()
-        if data:
-            code, ev_type, value = map(int, data.decode().split())
-            print('mouse', code, ev_type, value)
+async def keyboard_client(code, ev_type, value):
+    print('keyboard', code, ev_type, value)
+    key = ecodes.KEY[code]
+    print(key)
+    if key not in keymap:
+        return
 
-            if code == 0:
-                mouse.move(value, 0)
-            elif code == 1:
-                mouse.move(0, value)
-            elif code == 272 and value == 1:
-                mouse.press(Button.left)
-            elif code == 272 and value == 0:
-                mouse.release(Button.left)
-            elif code == 273 and value == 1:
-                mouse.press(Button.right)
-            elif code == 273 and value == 0:
-                mouse.release(Button.right)
-            elif code == 274 and value == 1:
-                mouse.press(Button.middle)
-            elif code == 274 and value == 0:
-                mouse.release(Button.middle)
-            elif code == 8 and value == 1:
-                mouse.scroll(0, 10)
-            elif code == 8 and value == -1:
-                mouse.scroll(0, -10)
+    elif value == 1:
+        keyboard.press(keymap[key])
+    elif value == 0:
+        keyboard.release(keymap[key])
+
+
+async def mouse_client(code, ev_type, value):
+    if code == 0:
+        mouse.move(value, 0)
+    elif code == 1:
+        mouse.move(0, value)
+    elif code == 272 and value == 1:
+        mouse.press(Button.left)
+    elif code == 272 and value == 0:
+        mouse.release(Button.left)
+    elif code == 273 and value == 1:
+        mouse.press(Button.right)
+    elif code == 273 and value == 0:
+        mouse.release(Button.right)
+    elif code == 274 and value == 1:
+        mouse.press(Button.middle)
+    elif code == 274 and value == 0:
+        mouse.release(Button.middle)
+    elif code == 8 and value == 1:
+        mouse.scroll(0, 10)
+    elif code == 8 and value == -1:
+        mouse.scroll(0, -10)
 
 
 async def start():
-    reader, writer = await asyncio.open_connection(sys.argv[1], sys.argv[2])
-    line = await reader.readline()
-    device_type = line.decode().strip()
-    print(device_type)
-    if device_type == 'keyboard':
-        await keyboard_client(reader)
-    elif device_type == 'mouse':
-        await mouse_client(reader)
-    else:
-        print('unknown device or bad line')
+    host, port = sys.argv[1], sys.argv[2]
+    print(f"connecting to {host}:{port}")
+    reader, writer = await asyncio.open_connection(host, port)
+    while True:
+        data = await reader.readline()
+        device_type, code, ev_type, value = map(int, data.decode().split())
+        print(device_type, code, ev_type, value)
+        if device_type == 0:
+            await keyboard_client(code, ev_type, value)
+        elif device_type == 1:
+            await mouse_client(code, ev_type, value)
+        else:
+            print('unknown device or bad line')
 
 
 def main():
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import evdev
 import asyncio
+from selectors import DefaultSelector, EVENT_READ
 
 
 class Grabbed:
@@ -11,85 +12,65 @@ class Grabbed:
 
 
 class InputHandler:
-    dev = None
-    dev_type = None
 
-    def __init__(self, dev, dev_type, grabbed):
-        self.dev = dev
-        self.dev_type = dev_type
-        self.grabbed = grabbed
+    def __init__(self):
+        self.grabbed = Grabbed()
         self.mouse_grabbed = False
+        self.queue = asyncio.Queue()
 
-    async def read_keyboard(self):
-        async for ev in self.dev.async_read_loop():
-            if ev.type == 1:
-                if ev.code == 70 and ev.value == 1:
-                    if not self.grabbed:
-                        self.dev.grab()
-                        self.grabbed.enabled = True
-                    else:
-                        self.dev.ungrab()
-                        self.grabbed.enabled = False
+    async def read_device(self, dev, dev_type):
+        async for ev in dev.async_read_loop():
+            if dev_type == 0:
+                if ev.type == 1:
+                    if ev.code == 70 and ev.value == 1:
+                        if not self.grabbed:
+                            dev.grab()
+                            self.grabbed.enabled = True
+                        else:
+                            dev.ungrab()
+                            self.grabbed.enabled = False
 
-                yield ev.code, ev.type, ev.value
+            elif dev_type == 1:
+                if self.grabbed:
+                    if not self.mouse_grabbed:
+                        dev.grab()
+                        self.mouse_grabbed = True
+                else:
+                    if self.mouse_grabbed:
+                        dev.ungrab()
+                        self.mouse_grabbed = False
 
-    async def read_mouse(self):
-        async for ev in self.dev.async_read_loop():
-            if self.grabbed:
-                if not self.mouse_grabbed:
-                    self.dev.grab()
-                    self.mouse_grabbed = True
-            else:
-                if self.mouse_grabbed:
-                    self.dev.ungrab()
-                    self.mouse_grabbed = False
-
-            yield ev.code, ev.type, ev.value
+            await self.queue.put((dev_type, ev.code, ev.type, ev.value))
 
     async def server_callback(self, reader, writer):
         print("client connected")
-        writer.write(f"{self.dev_type}\n".encode())
-        if self.dev_type == "keyboard":
-            async for code, ev_type, value in self.read_keyboard():
-                if self.grabbed:
-                    writer.write(f"{code} {ev_type} {value}\n".encode())
-
-        elif self.dev_type == "mouse":
-            async for code, ev_type, value in self.read_mouse():
-                if self.grabbed:
-                    writer.write(f"{code} {ev_type} {value}\n".encode())
+        while True:
+            dev_type, code, ev_type, value = await self.queue.get()
+            if self.grabbed:
+                writer.write(f"{dev_type} {code} {ev_type} {value}\n".encode())
 
 
-def main():
+async def main():
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
     found_devices = [dev for dev in devices if 'Logitech' in dev.name]
 
     print(found_devices)
 
-    loop = asyncio.get_event_loop()
-
-    grabbed = Grabbed()
-
-    for dev in found_devices:
-        caps = dev.capabilities()
+    inpt = InputHandler()
+    for d in found_devices:
+        caps = d.capabilities()
         dev_buttons = caps[1]
 
-        # if it has the escape key, it's a keyboard
         if 1 in dev_buttons:
-            asyncio.ensure_future(
-                asyncio.start_server(
-                    InputHandler(dev, "keyboard", grabbed).server_callback, '0.0.0.0', 5842, loop=loop
-                )
-            )
+            asyncio.ensure_future(inpt.read_device(d, 0))
         else:
-            asyncio.ensure_future(
-                asyncio.start_server(
-                    InputHandler(dev, "mouse", grabbed).server_callback, '0.0.0.0', 5843, loop=loop
-                )
-            )
+            asyncio.ensure_future(inpt.read_device(d, 1))
 
-    loop.run_forever()
+    server = await asyncio.start_server(inpt.server_callback, '0.0.0.0', 5842)
+
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
